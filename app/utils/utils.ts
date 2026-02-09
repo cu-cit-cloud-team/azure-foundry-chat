0108fa835b9ea33cb11405fb3878375fc40bcc0b
Michael Sprague <mikesprague@gmail.com>
2026-02-09 14:39:18 -0500

    refactor(util/utils): add isDevelopment function

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
+export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
-}
+};
+
+export const isDevelopment = () =>
+  process.env.NODE_ENV === 'development' ||
+  location.hostname === 'localhost' ||
+  location.hostname === '127.0.0.1';
